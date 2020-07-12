fb1d7f01426f74f87c17480e86da235d0b397324
luis angel pfuño canales <alguien310599@gmail.com>
2020-07-12 15:22:01 -0500

    finish view/tableDocente admin

--- private/assets/js/mainAdmin.js
+++ private/assets/js/mainAdmin.js
@@ -1,12 +1,13 @@
 //console.log(idRegistro);
 const dataAditional=document.querySelector('#infoAdicional');
 const pagination=document.querySelector('#table-pagination-global');
+const search=document.querySelector('#myInput');
 let ArregloJson=[];
-
+let datasBrowser=[];
 /* reiniciamos el arreglo de obj JSON */
-const resetDataJson=()=>{
-  while(ArregloJson.length>0){
-    ArregloJson.pop();
+const resetDataJson=(arr=ArregloJson)=>{
+  while(arr.length>0){
+    arr.pop();
   }
 }
 
@@ -25,19 +26,19 @@ const createDataByJson=(id,cod,rol,nombre,apellido,email,phone)=>{
 }
 
 /* imprimimos datos en tabla */
-const printByRange=(initial=0)=>{
+const printByRange=(initial=0,arr=ArregloJson)=>{
   let final=initial+8;
-  let tamArr=ArregloJson.length;
+  let tamArr=arr.length;
   const rellenar=document.querySelector('#myTable');
   rellenar.innerHTML='';
   while(true){
     rellenar.innerHTML+=`
         <tr>
-          <td>${ArregloJson[initial].codigo}</td>
-          <td>${ArregloJson[initial].nombre}</td>
-          <td>${ArregloJson[initial].apellido}</td>
-          <td>${ArregloJson[initial].email}</td>
-          <td>${ArregloJson[initial].contacto}</td>
+          <td>${arr[initial].codigo}</td>
+          <td>${arr[initial].nombre}</td>
+          <td>${arr[initial].apellido}</td>
+          <td>${arr[initial].email}</td>
+          <td>${arr[initial].contacto}</td>
         </tr>
     `;
     initial++;
@@ -49,34 +50,69 @@ const printByRange=(initial=0)=>{
     }
   }
 }
-const printTable=(cantPrint=0)=>{
-    printByRange(cantPrint);
+const printTable=(cantPrint=0,arr=ArregloJson)=>{
+    printByRange(cantPrint,arr);
 }
 
 /* CREANDO LA PAGINACION DE LA TABLA DE DATOS */
-const createPaginationOftable=()=>{
-  let cantPagination=Math.ceil(ArregloJson.length/8);
-  pagination.innerHTML='';
-  pagination.innerHTML+=`
-    <li class="page-item disabled"><a class="page-link" href="#">Previous</a></li> `;
-    for(let i=0;i<cantPagination;i++){
+const createPaginationOftable=(Arreglo=ArregloJson,view=true)=>{
+  let cantPagination=Math.ceil(Arreglo.length/8);
+  if(view){
+      pagination.innerHTML='';
       pagination.innerHTML+=`
-        <li class="page-item ${(i==0)?'active':''}"><a class="page-link" href="#">${(i+1)}</a></li>
-      `;
-    }
-  pagination.innerHTML+=`
-    <li class="page-item"><a class="page-link" href="#">Next</a></li> `;
+        <li class="page-item disabled"><a class="page-link" href="#">Previous</a></li> `;
+        for(let i=0;i<cantPagination;i++){
+          pagination.innerHTML+=`
+            <li class="page-item ${(i==0)?'':''}"><a class="page-link" href="#">${(i+1)}</a></li>
+          `;
+        }
+      pagination.innerHTML+=`
+      <li class="page-item"><a class="page-link" href="#">Next</a></li> `;
+  }
+return cantPagination;
 }
 
 /* METODO DE BUSQUEDA DINAMICA */
-$(document).ready(function(){
+/* $(document).ready(function(){
   $("#myInput").on("keyup", function() {
     var value = $(this).val().toLowerCase();
     $("#myTable tr").filter(function() {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
-});
+}); */
+
+/**
+ * METODO DE BUSQUEDA POR ARREGLO JSON
+ */
+const busquesaJson=()=>{
+  let textSearch=search.value.toLowerCase();
+  resetDataJson(datasBrowser);
+  for(let v of ArregloJson){
+    const codigo=v.codigo.toLowerCase();
+    const nombre=v.nombre.toLowerCase();
+    const apellido=v.apellido.toLowerCase();
+    const email=v.email.toLowerCase();
+    const contacto=v.contacto.toLowerCase();
+    if(codigo.indexOf(textSearch)!=-1 || nombre.indexOf(textSearch)!=-1 || apellido.indexOf(textSearch)!=-1
+      || email.indexOf(textSearch)!=-1 || contacto.indexOf(textSearch)!=-1
+    ){
+      datasBrowser.push(v);
+    }
+  }
+  if(datasBrowser.length>0){
+    if(datasBrowser.length<=8){
+      printTable(0,datasBrowser);
+      createPaginationOftable(datasBrowser);
+    }else{
+      printTable(0,datasBrowser);
+      createPaginationOftable(datasBrowser);
+    }
+  }else{
+    console.log("no hay data");
+    createPaginationOftable(datasBrowser);
+  }
+}
 
 
 /**
@@ -143,6 +179,7 @@ const loader=()=>{
 }
 loader();
 
+
 if(dataAditional!==null){
   dataAditional.addEventListener('click',e=>{
     e.preventDefault();
@@ -152,19 +189,42 @@ if(dataAditional!==null){
         text:'Datos guardados.'
     })
   })
-}else if(pagination!==null){
-pagination.addEventListener('click',e=>{
-  e.preventDefault();
-  console.log(e.target.textContent);
-  
-  let option=e.target.textContent;
-  if(option==='1'){
-    printTable(0);
-  }else if(option==='2'){
-    printTable(8);
-/*     e.target.classList.add('bg-primary');
-    e.target.classList.add('text-white'); */
-  }
-})
 }
-console.log(pagination.children);
+if(pagination!==null){
+  pagination.addEventListener('click',e=>{
+    e.preventDefault();
+    //console.log(e.target.textContent);
+    //console.log(createPaginationOftable(ArregloJson,false));
+    let numberPagination=1;
+    let rangePrintPagination=0;
+    let numberPress=parseInt(e.target.textContent);
+
+    let cant=createPaginationOftable(ArregloJson,false);
+    if(datasBrowser.length>0){
+      cant=createPaginationOftable(datasBrowser,false);
+      while(numberPagination<=cant){
+        if(numberPress===numberPagination){
+          printTable(rangePrintPagination,datasBrowser);
+          break;
+        }
+        rangePrintPagination+=8;
+        numberPagination++;
+      }
+    }else{
+      while(numberPagination<=cant){
+        if(numberPress===numberPagination){
+          printTable(rangePrintPagination);
+        }
+        rangePrintPagination+=8;
+        numberPagination++;
+      }
+    }
+
+  })
+}
+if(search!==null){
+  search.addEventListener('keyup',e=>{
+    e.preventDefault();
+    busquesaJson();
+  })
+}
--- public/login.php
+++ public/login.php
@@ -26,11 +26,11 @@
 
             <form action="index.php" method="post" enctype="multipart/form-data">
                 <div class="form-group">
-                    <input type="text" class="form-control" name="id" placeholder="Codigo">
+                    <input type="text" class="form-control" required name="id" placeholder="Codigo">
                 </div>
 
                 <div class="form-group">
-                    <input type="password" class="form-control" name="pass" placeholder="Password">
+                    <input type="password" class="form-control"  required name="pass" placeholder="Password">
                 </div>
 
                 <div class="form-group">
